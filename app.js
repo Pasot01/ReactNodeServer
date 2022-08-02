ed2d00bbf30f96e1c9c3588412399de5f229031f
Pasot01 <contactmyphone14@gmail.com>
2022-08-02 18:26:33 +0200

    Configuration et connection à MongoDB avec Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('Hello serveur Node!');
-});
+mongoose
+    .connect(`mongodb+srv://Pst:VToqLPifOQYg56r0@cluster0.r716vhi.mongodb.net/auth?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(5000);
+        console.log('Database is connected! Listening to localhost 5000');
+    })
+    .catch((err) => console.log(err));
 
-app.listen(5000, () => {
-    console.log('Listening to localhost 5000');
-});
