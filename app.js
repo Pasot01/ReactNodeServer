f09361e9dd92fb9d97380618a458f19fa0e80f04
Pasot01 <contactmyphone14@gmail.com>
2022-08-02 20:18:10 +0200

    Ajout de signup, route et controller. Test d'ajout d'un user, email et password avec Postman

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const router = require('./routes/user-routes');
 
 const app = express();
-
+app.use(express.json());
 app.use('/api', router);
 
 mongoose
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const {signup} = require('../controllers/user-controller');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('Hello World')
-});
+// router.get('/', (req, res, next) => {
+//     res.send('Hello World')
+// });
+
+router.post('/signup', signup);
 
 module.exports = router;
