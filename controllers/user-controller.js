2aa5e1c2e1b504cff06ad610a0af3c7a1f854c77
Pasot01 <contactmyphone14@gmail.com>
2022-08-04 15:29:03 +0200

    Amélioration de user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,23 @@
 const User = require('../model/User');
 
 const signup = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        console.log(err);
+    }
+    if (existingUser) {
+        return res
+            .status(400)
+            .json({ message: "User already exists!" });
+    }
+
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+        name,
+        email,
+        password
     });
     try {
         await user.save();
