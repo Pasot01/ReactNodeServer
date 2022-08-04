5dde4cd6ecf5f5800a28defc54e3da046313faae
Pasot01 <contactmyphone14@gmail.com>
2022-08-04 15:45:58 +0200

    Ajout du module bcryptjs dans user-controller pour les mots de passe

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -11,13 +12,15 @@ const signup = async (req, res, next) => {
     if (existingUser) {
         return res
             .status(400)
-            .json({ message: "User already exists!" });
+            .json({ message: "User already exists! Login Instead" });
     }
+
+    const hashedPassword = bcrypt.hashSync(password);
 
     const user = new User({
         name,
         email,
-        password
+        password: hashedPassword,
     });
     try {
         await user.save();
